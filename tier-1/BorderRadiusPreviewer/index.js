04bb2e3d7c767269e0e55901327eb277e73c1e94
Home Desktop-WSL Ubuntu <26182964+ballooner@users.noreply.github.com>
2024-12-12 16:28:34 -0500

    Added copy CSS feature

--- tier-1/BorderRadiusPreviewer/index.js
+++ tier-1/BorderRadiusPreviewer/index.js
@@ -1,6 +1,8 @@
 const displayBox = document.querySelector("#display-box");
 const elements = Array.from(document.querySelectorAll(".controller"));
+const copyButton = document.querySelector("#copy-button");
 
+//Turn the input's into a string
 function parseElements(controllerArray) {
     let borderString = "";
 
@@ -11,11 +13,19 @@ function parseElements(controllerArray) {
     return borderString;
 }
 
+//Change the style of the box
 function controllerHandler(e) {
     displayBox.style.borderRadius = parseElements(elements);
     console.log("ran");
 }
 
+function copyCSS(e) {
+    navigator.clipboard.writeText("border-radius: " + parseElements(elements));
+}
+
+//Add events to the inputs
 for (let i = 0; i < elements.length; i++) {
     elements[i].addEventListener("input", controllerHandler);
 }
+
+copyButton.addEventListener("click", copyCSS);
