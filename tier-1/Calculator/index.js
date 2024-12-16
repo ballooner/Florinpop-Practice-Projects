f7e3c91d8729cf496342a089ab9598494f3c2e10
Home Desktop-WSL Ubuntu <26182964+ballooner@users.noreply.github.com>
2024-12-16 14:04:43 -0500

    Added JS for buttons and some math operations

---
+++ tier-1/Calculator/index.js
@@ -0,0 +1,86 @@
+//Get HTML elements into variables
+let display = document.querySelector("#display-text");
+let buttons = document.querySelectorAll(".row button");
+
+let memoryStack = [];
+
+//Verify display text isn't longer than 8 numbers
+function verifyDisplay() {
+    if (display.textContent.length >= 8) {
+        return false;
+    }
+
+    return true;
+}
+
+function evaluateStack() {
+    let firstNum = Number(memoryStack[0]);
+    let operation = memoryStack[1];
+    let secondNum = Number(memoryStack[2]);
+    let finalVal = 0;
+
+    if (operation === "%") {
+        finalVal = firstNum % secondNum;
+    } else if (operation === "/") {
+        finalVal = firstNum / secondNum;
+    } else if (operation === "x") {
+        finalVal = firstNum * secondNum;
+    } else if (operation === "-") {
+        finalVal = firstNum - secondNum;
+    } else if (operation === "+") {
+        finalVal = firstNum + secondNum;
+    }
+
+    
+    display.textContent = finalVal;
+    memoryStack = [];
+}
+
+//Handler for number buttons
+function numberHandler(e) {
+    let buttonText = e.target.textContent;
+    
+    if (verifyDisplay() === false) {
+        return;
+    }
+
+    if (display.textContent === "0") {
+        display.textContent = buttonText;
+    } else {
+        display.textContent += buttonText;
+    }
+}
+
+//Handler for math operations
+function mathOperationHandler(e) {
+    let buttonText = e.target.textContent;
+    let displayText = display.textContent;
+
+    if (memoryStack.length === 0 && buttonText !== "=") {
+        memoryStack.push(displayText);
+        memoryStack.push(buttonText);
+        display.textContent = "";
+        console.log(memoryStack.length);
+    } else if (memoryStack.length === 2 && buttonText === "=") {
+        memoryStack.push(displayText);
+        evaluateStack();
+    } else if (memoryStack.length === 2) {
+        memoryStack.push(displayText);
+        evaluateStack();
+        memoryStack.push(display.textContent);
+        memoryStack.push(buttonText);
+        display.textContent = "";
+    }
+}
+
+function memoryOperationHandler(e) {
+
+}
+
+for (let i = 0; i < buttons.length; i++) {
+    if (buttons[i].textContent.match(/[1-9]/) !== null) {
+        buttons[i].addEventListener("click", numberHandler);
+    } else if (buttons[i].textContent.match(/[%/x\-+=]/) !== null) {
+        buttons[i].addEventListener("click", mathOperationHandler);
+    }
+}
